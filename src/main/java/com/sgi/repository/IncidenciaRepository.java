603c804a1cadf3b5f545ee55927d16adb4c0e42b
ktvzh <kriss.angela0@gmail.com>
2026-05-28 00:08:47 -0500

    Add method to find Incidencia by estado

--- src/main/java/com/sgi/repository/IncidenciaRepository.java
+++ src/main/java/com/sgi/repository/IncidenciaRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IncidenciaRepository extends JpaRepository<Incidencia, Long> {
+  List<Incidencia> findByEstado(String estado);
 }
