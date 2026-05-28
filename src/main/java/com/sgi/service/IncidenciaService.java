d5d678e93417d113d6a3ef00e8083d095ba67d14
DiegoLSC <diegosotelocaceres@gmail.com>
2026-05-27 20:37:43 -0500

    actualizacion de incidencia service

--- src/main/java/com/sgi/service/IncidenciaService.java
+++ src/main/java/com/sgi/service/IncidenciaService.java
@@ -1,10 +1,11 @@
 package com.sgi.service;
 
 import com.sgi.model.Incidencia;
+import com.sgi.model.Usuario;
 import com.sgi.repository.IncidenciaRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 public class IncidenciaService {
@@ -12,11 +13,8 @@ public class IncidenciaService {
     @Autowired
     private IncidenciaRepository incidenciaRepository;
 
-    public List<Incidencia> obtenerTodas() {
-        return incidenciaRepository.findAll();
-    }
-
-    public Incidencia guardarIncidencia(Incidencia incidencia) {
-        return incidenciaRepository.save(incidencia);
+    // Obtener las incidencias de un docente en específico
+    public List<Incidencia> obtenerPorUsuario(Usuario usuario) {
+        return incidenciaRepository.findByUsuario(usuario);
     }
 }
