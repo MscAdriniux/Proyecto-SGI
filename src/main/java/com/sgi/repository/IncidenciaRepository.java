939b4050e25f83f9e2066bb4785657a55a98bc56
DiegoLSC <diegosotelocaceres@gmail.com>
2026-05-27 20:37:54 -0500

    actualizacion de incidencia repository

--- src/main/java/com/sgi/repository/IncidenciaRepository.java
+++ src/main/java/com/sgi/repository/IncidenciaRepository.java
@@ -1,9 +1,15 @@
 package com.sgi.repository;
 
 import com.sgi.model.Incidencia;
+import com.sgi.model.Usuario;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IncidenciaRepository extends JpaRepository<Incidencia, Long> {
+public interface IncidenciaRepository extends JpaRepository<Incidencia, Integer> {
+    
+    // Spring Boot crea la consulta SQL automáticamente gracias a este nombre
+    List<Incidencia> findByUsuario(Usuario usuario);
+    
 }
