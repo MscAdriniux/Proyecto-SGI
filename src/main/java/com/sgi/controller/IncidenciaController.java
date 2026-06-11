4bca5c9ce8a448d3bc8d9019f93b1c89435582a5
ktvzh <kriss.angela0@gmail.com>
2026-06-10 20:21:53 -0500

    Add Logger for IncidenciaController

--- src/main/java/com/sgi/controller/IncidenciaController.java
+++ src/main/java/com/sgi/controller/IncidenciaController.java
@@ -1,6 +1,7 @@
 package com.sgi.controller;
 import com.sgi.service.ExcelService;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
